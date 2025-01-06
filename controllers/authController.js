f4f5b8fdbd1a9a18c8a743fac820f55bc0381bc9
githubscientist <sathish.datascientist@gmail.com>
2025-01-06 18:48:46 +0530

    login endpoint done!

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../utils/config');
 
 const authController = {
     register: async (request, response) => {
@@ -14,14 +17,50 @@ const authController = {
                 return response.status(400).json({ message: 'Email already in use' });
             }
 
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
             // Create a model object
-            const newUser = new User({ name, email, password });
+            const newUser = new User({ name, email, password: hashedPassword });
 
             // Save the user to the database
             await newUser.save();
 
             // Send a response
             response.status(201).json({ message: 'Registration successful' });
+        } catch (error) {
+            response.status(500).json({ message: error.message });
+        }
+    },
+    login: async (request, response) => {
+        try {
+            const { email, password } = request.body;
+
+            // find the user with the email
+            const user = await User.findOne({ email });
+
+            // if the user does not exist, send a response
+            if (!user) {
+                return response.status(404).json({ message: 'User not found' });
+            }
+
+            // compare the password
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            // if the password does not match, send a response
+            if (!isMatch) {
+                return response.status(400).json({ message: 'Invalid credentials' });
+            }
+
+            // create a token
+            const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '3h' });
+
+            // send the token to the http only cookie
+            response.cookie('token', token, { httpOnly: true });
+
+            // send a response
+            response.status(200).json({ message: 'Login successful ' });
+
         } catch (error) {
             response.status(500).json({ message: error.message });
         }
